497fe9e65f5ffe55d493f7b7fdab8554b6425a42
Vic <vicmisanu63@gmail.com>
2024-08-04 20:28:12 +0200

    middleware for auth, user connected

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -3,15 +3,82 @@ import { createAccountValidator } from '../validators/auth.js'
 import User from '#models/user'
 import hash from '@adonisjs/core/services/hash'
 
-// import User from '../models/user.js'
-// import Account from '../models/account.js'
-
+const tweets = [
+  {
+    id: 1,
+    name: 'CNN',
+    username: '@CNN',
+    tweetAvatar: 'images/tweet-profile-photo.png',
+    date: '7m',
+    text: 'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
+    comments: 57,
+    retweets: 144,
+    likes: 184,
+    shares: 0,
+    verified: true,
+  },
+  {
+    id: 2,
+    name: 'The New York Times',
+    username: '@nytimes',
+    date: '2h',
+    tweetAvatar: 'images/nytimes-avatar.png',
+    text: 'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land.',
+    image: 'images/tweet-image.png',
+    comments: 19,
+    retweets: 48,
+    likes: 484,
+    shares: 0,
+    verified: true,
+  },
+  {
+    id: 3,
+    name: 'Tweeter',
+    date: 'Oct 29',
+    username: '@twitter',
+    tweetAvatar: 'images/tweeter-avatar.png',
+    text: 'BIG NEWS lol jk still Twitter',
+    comments: '6.8K',
+    retweets: '36.6K',
+    likes: '267.1K',
+    shares: 0,
+    verified: true,
+  },
+  {
+    id: 4,
+    name: 'Tweeter',
+    date: 'Oct 04',
+    username: '@twitter',
+    tweetAvatar: 'images/tweeter-avatar.png',
+    text: 'hello literally everyone',
+    comments: '116.7K',
+    retweets: '785.5K',
+    likes: '3.3M',
+    shares: 0,
+    verified: true,
+  },
+  {
+    id: 5,
+    name: 'Twitter',
+    username: '@twitter',
+    date: '04 Oct',
+    tweetAvatar: 'images/tweeter-avatar.png',
+    text: 'hello literally everyone',
+    image: 'images/tweet-image.png',
+    comments: 19,
+    retweets: 48,
+    likes: 484,
+    shares: 0,
+    verified: true,
+  },
+]
 export default class AuthController {
   // BringToHomePage(ctx: HttpContext) {
   //   // return ctx.response.redirect().toRoute('home')
   //   return ctx.response.redirect().toRoute('welcome')
   // }
-  BringToTheFirstPage({ view }: HttpContext) {
+  BringToTheFirstPage({ view, auth }: HttpContext) {
+    const user = auth.user
     return view.render('pages/welcome')
   }
   async BringToLoginPage({ view }: HttpContext) {
@@ -20,33 +87,52 @@ export default class AuthController {
   SignUp({ view }: HttpContext) {
     return view.render('pages/signup')
   }
-  ShowTweet({ view }: HttpContext) {
-    return view.render('pages/home')
+  ShowTweet({ view, auth }: HttpContext) {
+    const user = auth.user
+    return view.render('pages/home', { tweets, user })
   }
 
-  async registerAccountInfo({ request, response }: HttpContext) {
+  async registerAccountInfo({ request, response, session }: HttpContext) {
     const { name, email, password } = await request.validateUsing(createAccountValidator)
 
     await User.create({ name, email, password })
+    session.flash('success', 'connexion reussie')
     return response.redirect('/login')
   }
 
-  async authenticateUser({ request, response }: HttpContext) {
-    const { email, password } = request.only(['email', 'password'])
+  async authenticateUser({ request, response, session, auth }: HttpContext) {
+    // const { email, password } = request.only(['email', 'password'])
     /**
      * Find a user by email. Return error if a user does
      * not exists
      */
-    let user = await User.findBy('email', email)
-    if (!user) {
-      response.abort('Invalid credentials')
+    // let user = await User.findBy('email', email)
+    // if (!user) {
+    //   response.flash('Invalid credentials')
+    // }
+    try {
+      const { email, password } = request.only(['email', 'password'])
+      let user = await User.findBy('email', email)
+
+      await hash.verify(password, password)
+      user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
+      console.log('is authenticated')
+      session.flash('success', 'connexion reussie')
+
+      return response.redirect('/home')
+    } catch (error) {
+      console.error(error)
+      session.flash('error', "L'email ou le mot de passe est incorrect")
+      return response.redirect().back()
     }
+
     /**
      * Verify the password using the hash service
      */
-    await hash.verify(password, password)
-    user = await User.verifyCredentials(email, password)
-    console.log('is authenticated')
-    return response.redirect('/home')
+    // await hash.verify(password, password)
+    // user = await User.verifyCredentials(email, password)
+    // console.log('is authenticated')
+    // return response.redirect('/home')
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,85 +8,19 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 const AuthController = () => import('../app/controllers/auth_controller.js')
 
-// const tweets = [
-//   {
-//     id: 1,
-//     name: 'CNN',
-//     username: '@CNN',
-//     tweetAvatar: 'images/tweet-profile-photo.png',
-//     date: '7m',
-//     text: 'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
-//     comments: 57,
-//     retweets: 144,
-//     likes: 184,
-//     shares: 0,
-//     verified: true,
-//   },
-//   {
-//     id: 2,
-//     name: 'The New York Times',
-//     username: '@nytimes',
-//     date: '2h',
-//     tweetAvatar: 'images/nytimes-avatar.png',
-//     text: 'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land.',
-//     image: 'images/tweet-image.png',
-//     comments: 19,
-//     retweets: 48,
-//     likes: 484,
-//     shares: 0,
-//     verified: true,
-//   },
-//   {
-//     id: 3,
-//     name: 'Tweeter',
-//     date: 'Oct 29',
-//     username: '@twitter',
-//     tweetAvatar: 'images/tweeter-avatar.png',
-//     text: 'BIG NEWS lol jk still Twitter',
-//     comments: '6.8K',
-//     retweets: '36.6K',
-//     likes: '267.1K',
-//     shares: 0,
-//     verified: true,
-//   },
-//   {
-//     id: 4,
-//     name: 'Tweeter',
-//     date: 'Oct 04',
-//     username: '@twitter',
-//     tweetAvatar: 'images/tweeter-avatar.png',
-//     text: 'hello literally everyone',
-//     comments: '116.7K',
-//     retweets: '785.5K',
-//     likes: '3.3M',
-//     shares: 0,
-//     verified: true,
-//   },
-//   {
-//     id: 5,
-//     name: 'Twitter',
-//     username: '@twitter',
-//     date: '04 Oct',
-//     tweetAvatar: 'images/tweeter-avatar.png',
-//     text: 'hello literally everyone',
-//     image: 'images/tweet-image.png',
-//     comments: 19,
-//     retweets: 48,
-//     likes: 484,
-//     shares: 0,
-//     verified: true,
-//   },
-// ]
-
-router.get('/', [AuthController, 'BringToTheFirstPage'])
-router.get('/welcome', [AuthController, 'BringToTheFirstPage']).as('welcome')
+router.get('/', [AuthController, 'BringToTheFirstPage']).use(middleware.guest())
+router
+  .get('/welcome', [AuthController, 'BringToTheFirstPage'])
+  .as('welcome')
+  .use(middleware.guest())
 router.get('/login', [AuthController, 'BringToLoginPage']).as('login')
 router.get('/signup', [AuthController, 'SignUp']).as('signup')
 router.post('/login', [AuthController, 'authenticateUser'])
 router.post('/signup', [AuthController, 'registerAccountInfo'])
-router.get('/tweet', [AuthController, 'ShowTweet'])
+router.get('/home', [AuthController, 'ShowTweet']).use(middleware.auth())
 // router
 //   .get('/home', async ({ view }) => {
 //     return view.render('pages/home', { tweets })
