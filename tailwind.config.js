49c01ab511869f066e4534b86c764e559d1660ca
Vic <vicmisanu63@gmail.com>
2024-10-18 14:52:34 +0200

    initializing the project and create the sign up interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.edge",
-    "./resources/**/*.{js,ts,jsx,tsx,vue}",
-  ],
+  content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        basicBlue: '#014284',
+        Bg1: '#000000',
+        Bg2: '#202327',
+        BgProfil: '#333639',
+      },
+    },
   },
   plugins: [],
 }
